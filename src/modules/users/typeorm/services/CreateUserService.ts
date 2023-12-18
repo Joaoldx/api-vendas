21b54310715d02093aade456667664ba8c90e8ad
João Domingos <j13domingos@gmail.com>
2023-12-18 00:26:47 -0300

    feat: criado serviço para criação do usuário

---
+++ src/modules/users/typeorm/services/CreateUserService.ts
@@ -0,0 +1,32 @@
+import { getCustomRepository } from 'typeorm';
+import AppError from '@shared/errors/AppError';
+import { UsersRepository } from '../repositories/UsersRepository';
+import User from '../entities/User';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateProductService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError('Já existe um email cadastrado');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+    return user;
+  }
+}
+
+export default CreateProductService;
