64b6b3f4338cbd4af5c02f586f7d01803a138fbc
João Domingos <j13domingos@gmail.com>
2023-12-18 00:45:10 -0300

    feat: criado controller de Users

---
+++ src/modules/users/typeorm/controllers/UsersController.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import ListUserService from '../services/ListUserService';
+import CreateUserService from '../services/CreateUserService';
+import DeleteUserService from '../services/DeleteUserService';
+
+export default class UserController {
+  public async index(request: Request, response: Response) {
+    const listUser = new ListUserService();
+
+    const users = await listUser.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response) {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    return response.json(user);
+  }
+
+  public async delete(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const deleteUser = new DeleteUserService();
+
+    const user = await deleteUser.execute({
+      id,
+    });
+
+    return response.json(user);
+  }
+}
