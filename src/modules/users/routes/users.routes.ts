80bacc44cbaa61d88ab0ca776ae4e7a6bee996bd
João Domingos <j13domingos@gmail.com>
2023-12-21 15:19:15 -0300

    feat: criado as rotas de usuário

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import UserController from '../controllers/UsersController';
+
+const usersRouter = Router();
+const usersController = new UserController();
+
+usersRouter.get('/', usersController.index);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+usersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  usersController.delete,
+);
+
+export default usersRouter;
