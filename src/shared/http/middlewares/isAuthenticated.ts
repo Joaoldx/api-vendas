c81b14e34a405a8564991f288082a75907b1fe14
João Domingos <j13domingos@gmail.com>
2023-12-21 23:22:31 -0300

    feat: adicionado id no payload

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,6 +3,12 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '@config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAutenticated(
   request: Request,
   response: Response,
@@ -17,7 +23,13 @@ export default function isAutenticated(
   const token = authHeader.split(' ')[1];
 
   try {
-    const decodeToken = verify(token, authConfig.jwt.secret);
+    const decodedToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodedToken as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
 
     return next();
   } catch (e) {
