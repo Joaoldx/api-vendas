9d51a69ae55520bd33b659b068c7a3580bb9645a
João Domingos <j13domingos@gmail.com>
2024-01-03 22:17:40 -0300

    feat: adiciona as novas rotas no router principal

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,4 +1,6 @@
 import productsRouter from '@modules/products/routes/products.routes';
+import passwordRouter from '@modules/users/routes/password.routes';
+import resetRouter from '@modules/users/routes/reset.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import usersRouter from '@modules/users/routes/users.routes';
 import { Router } from 'express';
@@ -8,5 +10,7 @@ const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/password', passwordRouter);
+routes.use('/password', resetRouter);
 
 export default routes;
