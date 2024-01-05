4e645f3784c22ce0aadc6c0d3447bd2deba477e2
João Domingos <j13domingos@gmail.com>
2024-01-05 00:14:55 -0300

    feat: realiza troca de senha passando o token para uma rota da aplicação frontend

--- src/modules/users/typeorm/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/typeorm/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { getCustomRepository } from 'typeorm';
+import path from 'path';
 import AppError from '@shared/errors/AppError';
 import { UsersRepository } from '../repositories/UsersRepository';
 import { UserTokenRepository } from '../repositories/UserTokenRepository';
@@ -19,11 +20,29 @@ class SendForgotPasswordEmailService {
       throw new AppError('Email não cadastrado');
     }
 
-    const token = await usersTokenRepository.generate(user.id);
+    const { token } = await usersTokenRepository.generate(user.id);
+
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        file: forgotPasswordTemplate,
+        variables: {
+          name: user.name,
+          link: `http://localhost:3000/reset_password?token=${token}`,
+        },
+      },
     });
   }
 }
