9c4bc31ea19db714b98fc6ae55e6cce483fbbdbe
João Domingos <j13domingos@gmail.com>
2024-01-19 23:19:06 -0300

    feat: criado interfaces customer dentro da área de domínio

---
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -0,0 +1,13 @@
+import { ICreateCustomer } from '../models/ICreateCustomer';
+import { ICustomer } from '../models/ICustomer';
+
+export interface ICustomersRepository {
+  findAll(): Promise<ICustomer[]>;
+  findByName(name: string): Promise<ICustomer | undefined>;
+  findById(id: string): Promise<ICustomer | undefined>;
+  findByEmail(email: string): Promise<ICustomer | undefined>;
+  create(data: ICreateCustomer): Promise<ICustomer>;
+  save(customer: ICustomer): Promise<ICustomer>;
+  show(name: string, email: string): Promise<ICustomer>;
+  delete(customer: ICustomer): Promise<void>;
+}
