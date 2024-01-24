e326ae8dcf77ec01e71bfe14e5b385e3a81f81bc
João Domingos <j13domingos@gmail.com>
2024-01-23 23:58:25 -0300

    refactor: refatorado repositório de users de acordo com a nova versão do typeorm

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,14 +1,15 @@
-import { Repository, getRepository } from 'typeorm';
+import { Repository } from 'typeorm';
 import User from '../entities/User';
 import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
 import { ICreateUser } from '@modules/users/domain/models/ICreateUser';
 import { IUser } from '@modules/users/domain/models/IUser';
+import { dataSource } from '@shared/infra/typeorm';
 
 export class UsersRepository implements IUsersRepository {
   private ormRepository: Repository<User>;
 
   constructor() {
-    this.ormRepository = getRepository(User);
+    this.ormRepository = dataSource.getRepository(User);
   }
 
   public async findAll(): Promise<IUser[]> {
@@ -48,7 +49,7 @@ export class UsersRepository implements IUsersRepository {
     await this.ormRepository.delete(user);
   }
 
-  public async findByName(name: string): Promise<User | undefined> {
+  public async findByName(name: string): Promise<User | null> {
     const user = this.ormRepository.findOne({
       where: {
         name,
@@ -58,7 +59,7 @@ export class UsersRepository implements IUsersRepository {
     return user;
   }
 
-  public async findById(id: string): Promise<User | undefined> {
+  public async findById(id: string): Promise<User | null> {
     const user = this.ormRepository.findOne({
       where: {
         id,
@@ -68,7 +69,7 @@ export class UsersRepository implements IUsersRepository {
     return user;
   }
 
-  public async findByEmail(email: string): Promise<User | undefined> {
+  public async findByEmail(email: string): Promise<User | null> {
     const user = this.ormRepository.findOne({
       where: {
         email,
